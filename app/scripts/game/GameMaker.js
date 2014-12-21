0d297edbc4feada07c91fadf9d07e753b003b9df
jhenriquez <julio.m.henriquez@gmail.com>
2014-12-21 11:42:41 -0400

    Crated Game Object Stubs.

---
+++ app/scripts/game/GameMaker.js
@@ -0,0 +1,5 @@
+define(['game/GameStorage'], function (storage) {
+	return {
+		text:"I'm the GameMaker!"
+	};
+});
---
+++ app/scripts/game/GameStorage.js
@@ -0,0 +1,28 @@
+define(['vendors/modernizr'],function () {
+	function GameStorage() {
+		this.setCurrentPlayer = function () {
+
+		};
+
+		this.removeCurrentPlayer = function() {
+
+		};
+
+		this.currentPlayer = function () {
+
+		};
+
+		this.saveHighScore = function (player, score) {
+
+		};
+
+		this.getHighScores = function () {
+			return [];
+		};
+	}
+
+	GameStorage.prototype = Object.create(GameStorage.prototype);
+	GameStorage.constructor = GameStorage;
+
+	return new GameStorage();
+});
