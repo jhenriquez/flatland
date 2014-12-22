243ce9ed105093d333bbfc3d8d0b8cac536f01c0
jhenriquez <julio.m.henriquez@gmail.com>
2014-12-21 23:41:41 -0400

    Configured index and game routes.

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var express = require('express'),
 
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
+app.set('views', __dirname + '/app');
+
+app.get('/index', function (rq, rs) {
+	rs.render('index');
+});
+
+app.get('/game', function (rq, rs) {
+	rs.render('game');
+});
 
 app.use(express.static(__dirname + '/app'));
 
