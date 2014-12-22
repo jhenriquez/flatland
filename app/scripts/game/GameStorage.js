0659fc9bf484e72bd3c4fc9d29f75d5bd3fba1ae
jhenriquez <julio.m.henriquez@gmail.com>
2014-12-21 23:24:43 -0400

    Completed GameStorage module with tests.

--- app/scripts/game/GameStorage.js
+++ app/scripts/game/GameStorage.js
@@ -1,20 +1,55 @@
 define(['vendors/modernizr'],function () {
 	function GameStorage() {
-		this.setCurrentPlayer = function () {
-
+		this.setCurrentPlayer = function (playername) {
+			localStorage['Flatland.CurrentPlayer'] = playername;
 		};
 
-		this.currentPlayer = function () {
-				
-		};	
+		this.getCurrentPlayer = function () {
+			return localStorage['Flatland.CurrentPlayer'];
+		};
+
+		this.setScore = function (score) {
+			if (!isValidScore(score)) return;
+			localStorage['Flatland.Score'] = score;
+		};
+
+		this.getScore = function () {
+			return parseInt(localStorage['Flatland.Score']) || 0;
+		};
 
 		this.saveHighScore = function (player, score) {
-
+			if (!player) return;
+			var highScore = 'Flatland.HighScores.' + player;
+			if (!isValidScore(score)) return;
+			if ((localStorage[highScore] || 0) < score) {
+				localStorage[highScore] = score;
+			}
 		};
 
-		this.getHighScores = function () {
-			return [];
+		this.getHighScores = function (player) {
+			var scores = [];
+			for (var value in localStorage) {
+				var valid = value.match(/^Flatland.HighScores.(.+)$/);
+				if(valid) {
+					if (player && valid[1] === player) {
+						return [{ player: valid[1], score: parseInt(localStorage[valid[0]]) }];
+					}
+					scores.push({
+						player: valid[1],
+						score: parseInt(localStorage[valid[0]])
+					});
+				}
+			}
+			return scores;
 		};
+
+		function isValidScore(score) {
+			return !isNaN(parseInt(score));
+		}
+
+		if (!Modernizr.localstorage) {
+			throw new Error("This module requires a browser that supports localStorage. Don't expect the game to work properly.");
+		}
 	}
 
 	GameStorage.prototype = Object.create(GameStorage.prototype);
--- specs/GameStorage-specs.js
+++ specs/GameStorage-specs.js
@@ -1,7 +1,123 @@
-describe('GameMaker', function () {
+describe('GameStorage', function () {
 	describe('Test Setup', function () {
-		it('should load a GameStorage object into the window object.', function () {
-			window.GameStorage.should.be.ok();
+		it('should load a GameStorage subject into the window.', function () {
+			GameStorage.should.be.ok();
+		});
+	});
+
+	describe('setCurrentPlayer', function () {
+		beforeEach(function() {
+			localStorage.clear();
+		});
+		it('should set the current player by name.', function () {
+			localStorage.should.have.length(0);
+			GameStorage.setCurrentPlayer('Batida');
+			localStorage.should.have.length(1);
+		});
+		it('shoul be idempotent.', function () {
+			for(var i = 0; i < 10; i++) {
+			  GameStorage.setCurrentPlayer('Batida');
+			}
+			localStorage.should.have.length(1);
+		});
+	});
+
+	describe('getCurrentPlayer', function () {
+		beforeEach(function () {
+			localStorage.clear();
+		});
+		it('should be able to trieve the current playername.', function () {
+			localStorage.should.have.length(0);
+			GameStorage.setCurrentPlayer('batida');
+			localStorage.should.have.length(1);
+			GameStorage.getCurrentPlayer().should.be.eql('batida');
+		});
+	});
+
+	describe('getScore', function () {
+		beforeEach(function () {
+			localStorage.clear();
+		});
+		it('should return 0 when no previous score exists.', function () {
+			localStorage.should.have.length(0);
+			GameStorage.getScore().should.eql(0);
+		});
+	});
+
+	describe('setScore', function () {
+		beforeEach(function () {
+			localStorage.clear();
+		});
+		it('should set a score.', function () {
+			localStorage.should.have.length(0);
+			GameStorage.setScore(10);
+			GameStorage.getScore().should.be.eql(10);
+		});
+		it('should ignore an invalid input.', function () {
+			GameStorage.setScore(10);
+			GameStorage.setScore('InvalidString');
+			GameStorage.getScore().should.be.eql(10);
+		});
+	});
+
+	describe('saveHighScore', function () {
+		beforeEach(function () {
+			localStorage.clear();
+		});
+		it('should save a HighScore.', function () {
+			localStorage.should.have.length(0);
+			GameStorage.saveHighScore('batida', 10);
+			localStorage.should.have.length(1);
+		});
+		it('should ignore a score that is lower than the current one for a player.', function () {
+			localStorage.should.have.length(0);
+			GameStorage.saveHighScore('batida', 10);
+			GameStorage.saveHighScore('batida', 9);
+			GameStorage.getHighScores('batida').should.eql([{player: 'batida', score: 10 }]);
+		});
+		it('should ignore a call where no player has been supplied.', function () {
+			localStorage.should.have.length(0);
+			GameStorage.saveHighScore(undefined, 10);
+			localStorage.should.have.length(0);
+			GameStorage.saveHighScore('batida', 10);
+			localStorage.should.have.length(1);
+			GameStorage.saveHighScore(undefined, 10);
+		});
+		it('should be idempotent.', function () {
+			localStorage.should.have.length(0);
+			for(var i = 0; i < 5; i++) {
+			  GameStorage.saveHighScore(undefined, 10);
+			}
+			localStorage.should.have.length(0);
+			for(var i = 0; i < 5; i++) {
+			  GameStorage.saveHighScore('batida', 10);
+			}
+			localStorage.should.have.length(1);
+		});
+	});
+
+	describe('getHighScores', function () {
+		beforeEach(function () {
+			localStorage.clear();
+		});
+		it('should return an array.', function () {
+			GameStorage.getHighScores().should.be.a('Array');
+		});
+		it('should return an empty array when no high scores exist.', function () {
+			localStorage.should.have.length(0);
+			GameStorage.getHighScores().should.be.a('Array').and.have.length(0);
+		});
+		it('should return an array of objects with the player and score.', function () {
+			localStorage.should.have.length(0);
+			GameStorage.saveHighScore('batida', 10);
+			GameStorage.getHighScores().should.be.a('Array').and.have.length(1).and.eql([{player: 'batida', score: 10}]);
+		});
+		it('it returns only the results for a single player when a playername is suplied.', function () {
+			localStorage.should.have.length(0);
+			GameStorage.saveHighScore('batida', 10);
+			GameStorage.saveHighScore('Pablo', 10);
+			localStorage.should.have.length(2);
+			GameStorage.getHighScores('batida').should.be.a('Array').and.have.length(1).and.eql([{player: 'batida', score: 10}]);
 		});
 	});
 });
