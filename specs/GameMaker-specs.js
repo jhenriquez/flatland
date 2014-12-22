f535ec257e95fcda1d3b2ab94cc121342622d59a
jhenriquez <julio.m.henriquez@gmail.com>
2014-12-21 23:34:47 -0400

    Created GameMaker specs stubs.

---
+++ specs/GameMaker-specs.js
@@ -0,0 +1,11 @@
+describe('GameMaker', function () {
+	describe('Test Setup', function () {
+		it('should load a global GameMaker object.', function () {
+			GameMaker.should.be.ok();
+		});
+	});
+
+	describe('Module Construction', function () {
+		it('should ')
+	});
+});
