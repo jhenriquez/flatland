1d68ac41f4d5cb64fcacf197959b60599a63e252
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-12-23 09:51:49 -0400

    Index Form redirects to game when valid.

--- app/scripts/views/IndexViewModel.js
+++ app/scripts/views/IndexViewModel.js
@@ -1,30 +1,18 @@
 requirejs(['../rjsConfig',], function () {
 	requirejs(['game/GameStorage'], function (storage) {
-		function IndexViewModel(storage) {			
-			this.getName = function () {
-				return $('input[for="name"]').val();
+
+		function onSubmit (e) {
+			if (!$('input[for="name"]').val()) {
+				e.preventDefault();
+				$('#name-form-group').addClass('has-error');
+			} else {
+				storage.setCurrentPlayer(this.getName());
 			}
-
-			this.setName = function (name) {
-				if(name) {
-					$('input[for="name"]').val(name);	
-				}
-			}
-
-			this.onSubmit = function (e) {
-				e.preventDefault();  
-				if (!this.getName()) {
-					$('#name-form-group').addClass('has-error');
-				} else {
-					storage.setCurrentPlayer(this.getName());
-				}
-			}.bind(this);
-
-			$('form').submit(this.onSubmit);
-			$('.btn').click(this.onSubmit);
 		}
 
-		window.IndexViewModel = new IndexViewModel(storage);
+		$('form').submit(onSubmit);
+		$('.btn').click(onSubmit);
+
 		return window.IndexViewModel;
 	});
 });
