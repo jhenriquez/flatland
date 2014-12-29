907906ee0a35a22e01db3fc38fcf1d1e0821fb8f
jhenriquez <julio.m.henriquez@gmail.com>
2014-12-28 22:17:35 -0400

    Added High Scores to the front screen.

--- app/scripts/views/IndexViewModel.js
+++ app/scripts/views/IndexViewModel.js
@@ -12,5 +12,23 @@ requirejs(['../rjsConfig',], function () {
 
 		$('form').submit(onSubmit);
 		$('.btn').click(onSubmit);
+
+		var highScores = storage.getHighScores();
+
+		if (highScores.length === 0) {
+			return;
+		}
+
+		highScores.sort(function (a,b) { return b.score - a.score; });		
+
+		$('#score-board').append('<h3>High Scores</h3>');
+
+		_.each(highScores, function (hs) {
+			var alert =   '<div class="alert alert-success" role="alert">'
+						+ hs.player
+						+ '<span class="badge pull-right">'+ hs.score +'</span>'
+						+ '</div>'
+			$('#score-board').append(alert);
+		});
 	});
 });
